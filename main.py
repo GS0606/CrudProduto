f56a4c974d10e55a550f2d7f2a5e89489fa29325
Guilherme Santos <133273066+GS0606@users.noreply.github.com>
2024-02-28 21:15:17 -0300

    Add files via upload
    Crud finalizado

--- main.py
+++ main.py
@@ -24,11 +24,62 @@ class Dbprodutos:
         self.conexao.commit()
         self.desconectar()
     
+    def inserir_produto(self, nome , valor):
+        
+        self.conectar()
+        self.cursor.execute("INSERT INTO produtos (nome, valor) VALUES (?, ?)", (nome, valor))
+        
+        self.conexao.commit()
+        self.desconectar()
+    
+    def listar_produto(self):
+        
+        self.conectar()
+        self.cursor.execute("SELECT * FROM produtos")
+        produtos = self.cursor.fetchall()
+        self.conexao.commit()
+        self.desconectar()
+        return produtos
+    
+    def atualizar_produto(self, id, nome, valor):
+        self.conectar()
+        self.cursor.execute("UPDATE produtos SET nome=?, valor=? WHERE id= ?",(nome, valor, id) )
+        self.conexao.commit()
+        self.desconectar()
+        
+        
+    def deletar_produto(self, id):
+        self.conectar()
+        self.cursor.execute("DELETE FROM produtos WHERE id=?", (id,))
+        self.conexao.commit()
+        self.desconectar()
     
 teste = Dbprodutos()
-teste.criar_tb()
-  
-        
 
+
+# teste.criar_tb()
+
+
+# teste.inserir_produto("CIMENTO" ,61)
+
+produtos = teste.listar_produto()
+print("Lista de Produto:  ")
+for produto in produtos:
+    print(produto)
+
+
+# id_produto = 1
+# teste.atualizar_produto(id_produto, "BALDE", 29 )
+
+# produtos_atualizados = teste.listar_produto()
+# print("\nLista de produtos atualizados:")
+# for produto in produtos_atualizados:
+#     print(produto)
         
+        
+# remover_id = 10
+# teste.deletar_produto(remover_id)
+# print(f"\nProduto com ID {remover_id} deletado.")
+
+
     
