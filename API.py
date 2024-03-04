a92e99ecdeeaa55b767c51c78d0811a048d35b59
Guilherme Santos <133273066+GS0606@users.noreply.github.com>
2024-03-04 18:59:16 -0300

    Add files via upload
    API rodando sem estresse !

--- API.py
+++ API.py
@@ -1,57 +1,60 @@
 from flask import Flask, jsonify, request
+import sqlite3
 
 app = Flask(__name__)
 
+def conectar_bd():
+    return sqlite3.connect('loja_virtual10.db')
 
-produtos =  [
-    {
-        'id': 1,
-        'nome': 'Carrinho de mão',
-        'preco': 70
-    },
-    {
-        'id': 2,
-        'nome': 'Balde',
-        'preco': 20
-    },
-      {
-        'id': 3,
-        'nome': 'Cimento',
-        'preco': 75
-    }
-    
-]
-@app.route('/produtos',methods=['GET'])
-def buscar_produto():
+@app.route('/produtos', methods=['GET'])
+def buscar_produtos():
+    conexao = conectar_bd()
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM produto")
+    produtos = cursor.fetchall()
+    conexao.close()
     return jsonify(produtos)
 
-@app.route('/produtos/<int:id>',methods=['GET'])
-def buscar_produto_id(id):
-    for produto in produtos:
-       if produto.get('id') == id:
-           return jsonify(produto)
-       
-@app.route('/produtos/<int:id>',methods= ['PUT'])
-def editar_produto_id(id):
-    produto_alterado= request.get_json()
-    for indice,produto in enumerate(produtos):
-        if produto.get('id')== id:
-            produtos[indice].update(produto_alterado)
-            return jsonify(produtos[indice])
+@app.route('/produtos/<int:id>', methods=['GET'])
+def buscar_produto_por_id(id):
+    conexao = conectar_bd()
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM produto WHERE id=?", (id,))
+    produto = cursor.fetchone()
+    conexao.close()
+    if produto:
+        return jsonify(produto)
+    else:
+        return jsonify({"mensagem": "Produto não encontrado"}), 404
 
-@app.route('/produtos',methods=['POST'])
+@app.route('/produtos', methods=['POST'])
 def inserir_produto():
     novo_produto = request.get_json()
-    produtos.append(novo_produto)
-    
-    return jsonify(produtos)
+    conexao = conectar_bd()
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO produto (nome, valor) VALUES (?, ?)", (novo_produto['nome'], novo_produto['valor']))
+    conexao.commit()
+    conexao.close()
+    return jsonify({"mensagem": "Produto inserido com sucesso"})
 
-@app.route('/produtos/<int:id>',methods= ['DELETE'])
+@app.route('/produtos/<int:id>', methods=['PUT'])
+def editar_produto(id):
+    produto_alterado = request.get_json()
+    conexao = conectar_bd()
+    cursor = conexao.cursor()
+    cursor.execute("UPDATE produto SET nome=?, valor=? WHERE id=?", (produto_alterado['nome'], produto_alterado['valor'], id))
+    conexao.commit()
+    conexao.close()
+    return jsonify({"mensagem": "Produto atualizado com sucesso"})
+
+@app.route('/produtos/<int:id>', methods=['DELETE'])
 def excluir_produto(id):
-    for indice,produto in enumerate(produtos):
-        if produto.get('id') == id:
-            del produtos[indice]
-            
-    return jsonify(produtos)
-    
-app.run(port=5000,host='localhost',debug=True)
+    conexao = conectar_bd()
+    cursor = conexao.cursor()
+    cursor.execute("DELETE FROM produto WHERE id=?", (id,))
+    conexao.commit()
+    conexao.close()
+    return jsonify({"mensagem": "Produto excluído com sucesso"})
+
+if __name__ == '__main__':
+    app.run(port=5000, host='localhost', debug=True)
